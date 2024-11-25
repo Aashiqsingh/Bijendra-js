3f8d48989b930374f07832ea12617c860cb0ad4c
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-11-25 18:51:49 +0530

    basic , intro

---
+++ basic/basic1.js
@@ -0,0 +1,39 @@
+// console.log("Hello world!");
+// console.info("information function called")
+// console.warn("warning function called");
+// console.error("error function called");
+
+// dataTypes : number  , string , boolean , undefined , object
+
+
+
+a = 10;
+console.log("value of a = ",a);
+console.log("type of a = ",typeof(a));
+
+b = 12.5;
+console.log("value of b = ",b);
+console.log("type of b = ",typeof b);
+
+c = "ram";
+console.log("value of c = ",c);
+console.log("type of c = ",typeof c);
+
+isAlive = true;
+console.log("value of isAlive = ",isAlive);
+console.log("type of isAlive = ",typeof isAlive);
+
+var x;
+console.log("value of x = ",x);
+console.log("type of x = ",typeof x);
+
+var y = null;
+console.log("value of y = ",y);
+console.log("type of y = ",typeof y);
+
+
+
+
+
+
+
---
+++ basic/basic2.js
@@ -0,0 +1,64 @@
+// var a = 10;
+// console.log("outer value of a = ",a);
+
+// let b = 20;
+// console.log("outer value of b = ",b);
+
+
+
+// {
+//     console.log("inner value of a = ",a);
+//     console.log("inner value of b = ",b);
+
+
+//     var c = 30;
+//     console.log("inner value of c = ",c);
+
+//     let d = 40;
+//     console.log("inner value of d = ",d);
+    
+    
+    
+       
+// }
+
+// console.log("outer value of c = ",c);
+// console.log("outer value of d = ",d);
+
+
+// var : redclare , reassign possible here
+
+// var a = 10;
+// console.log("value of a = ",a);
+
+
+// var a = 20;
+// console.log("value of a = ",a);
+
+
+// let : only reassign possible here 
+
+
+// let a = 10;
+// console.log("value of a = ",a);
+
+// a = 20;
+// console.log("value of a = ",a);
+
+
+// const pi = 3.14;
+// console.log("value of pi = ",pi);
+
+// pi = 3.15;
+// console.log("value of pi = ",pi);
+
+
+var a = 10;
+console.log("value of a = ",a);
+
+
+let a = 20;
+console.log("value of a = ",a);
+
+
+
