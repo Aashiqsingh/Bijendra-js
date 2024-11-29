d2432b129cbb73262e4120704daba20d5cd30f36
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-11-29 18:49:19 +0530

    string

---
+++ string/string1.js
@@ -0,0 +1,91 @@
+var str = "Shree gurukrupa           vidya sankul";
+// var str2 = "Bijendra"
+console.log(str.length);
+
+
+
+var x = str.trim()
+// var x = str.trimEnd()
+// var x = str.trimStart()
+
+
+console.log(x.length);
+
+
+
+// var x = str.toLowerCase()
+// var x = str.toUpperCase()
+
+// console.log(x);
+
+
+// var x = str.split("")
+// var x = str.split(" ")
+// var x = str.split("vidya")
+
+// console.log(x);
+
+
+
+// var x = str.slice(1)
+// var x = str.slice(1,4)
+// var x = str.slice(-1)
+// var x = str.slice(-5,-2)
+
+
+
+
+// console.log(x);
+
+
+
+// var x = str.replace("g","royal")
+// var x = str.replace("u","r")
+// var x = str.replaceAll("u","r")
+// console.log(x);
+
+
+// var x = str.repeat(1)
+// console.log(x);
+
+
+// var x = str.indexOf("g")
+// console.log(x);
+
+
+// var x = str.includes("vidy")
+// console.log(x);
+
+
+
+
+// var x = str.endsWith("")
+// var x = str.startsWith("s")
+// console.log(x);
+
+
+
+
+// console.log(str.length);
+
+// var x = str.charAt(1)
+// console.log(x);
+
+// var x = str.charCodeAt(0)
+// console.log(x);
+
+
+// str.codePointAt()
+
+// var x = str2.concat(str)
+// console.log(x);
+
+// no1 = parseInt(prompt("Enter first number "))
+// no2 = parseInt(prompt("Enter second number "))
+
+// // console.log(no1 + no2);
+// // console.log("2" + "4");
+
+// console.log(no1 + no2);
+
+
