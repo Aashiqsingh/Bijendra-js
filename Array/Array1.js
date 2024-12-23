865835ae7150cd91f15ade2d4350ebc761bde547
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-12-23 17:57:38 +0530

    Array

---
+++ Array/Array1.js
@@ -0,0 +1,104 @@
+// array : hetrogenius : 
+
+// var arr = [23,45,true, "ram"]
+// console.log(arr);
+// console.log(arr.length);
+
+// var arr = [34,56,75,34,66,33,22,44];
+
+// console.log(arr);
+// console.log(arr[0]);
+// console.log(arr[1]);
+
+
+
+var arr = ["ram","lashman","hanuman","seeta","geeta","krishna","radha"]
+
+// arr.splice(1,0,"parshuram")
+// arr.splice(1,1,"parshuram")
+arr.splice(-2,2)
+
+
+console.log(arr);
+
+
+
+// var x = arr.slice(1,4)
+// console.log(x);
+
+
+// var x = arr.reverse()
+// console.log(x);
+
+// var x = arr.join("-")
+// var x = arr.join("$")
+// var x = arr.join(" ")
+
+
+// console.log(x);
+
+
+// var x = arr.includes("seet")
+// console.log(x);
+
+
+// var x = arr.indexOf("seeta")
+// console.log(x);
+
+
+
+// arr.push("pushpa")
+// arr.push("pushpa","srivalli","keshav")
+// console.log(arr);
+
+
+// arr.pop()
+
+// console.log(arr);
+
+// arr.unshift("pushpa")
+// arr.shift()
+// console.log(arr);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for(let i=0;i<arr.length;i++){
+
+//     console.log(arr[i].toUpperCase());
+    
+// }
+
+// for (const i of arr) {
+    
+//     console.log(i);
+    
+// }
+
+
